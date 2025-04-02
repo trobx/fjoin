5d32d38340b5d97148365e8245afd91570389211
Toby Robertson <toby.g.robertson@gmail.com>
2025-04-02 17:39:52 +0100

    Helpers (cosmetic tidying)

--- R/helpers.R
+++ R/helpers.R
@@ -3,51 +3,71 @@ check_TF <- function(x) {
   if (!x %in% c(TRUE, FALSE))
     stop(sprintf("Argument '%s' must be TRUE or FALSE", deparse(substitute(x))))
 }
-
 # ------------------------------------------------------------------------------
 check_arg_order <- function(x) {
   if (!x %in% c("x", "y"))
     stop(sprintf("Argument '%s' must be \"x\" or \"y\"", deparse(substitute(x))))
 }
-
 # ------------------------------------------------------------------------------
 check_mult <- function(x) {
   if (!x %in% c("all", "first", "last"))
     stop(sprintf("Argument '%s' must be \"all\", \"first\", or \"last\"", deparse(substitute(x))))
 }
-
 # ------------------------------------------------------------------------------
 check_nomatch <- function(x) {
   if (! (is.null(x) || x %in% c(NA, 0L)))
     stop(sprintf("Argument '%s' must be NA, NULL, or 0L", deparse(substitute(x))))
 }
-
 # ------------------------------------------------------------------------------
 check_input_class <- function(x) {
   # Check x is a list or data.frame (data.table etc.)
   if (!(is.data.frame(x) || is.list(x)))
     stop(sprintf("Argument '%s' must be a data.frame-like object or list", deparse(substitute(x))))
 }
-
 # ------------------------------------------------------------------------------
 shallow_DT <- function(x) {
   # Shallow-copy columns of a data.frame-like object (or list of vectors) into a new DT
   data.table::setDT(unclass(x))
 }
-
 # ------------------------------------------------------------------------------
-clean_up <- function(x) {
+clean_up <- function(x, pattern = "^fjoin") {
   # Drop any columns with name starting with "fjoin"
   # suppressWarnings() is innocuous and convenient
-  suppressWarnings(data.table::set(x, j = names(x)[grepl("^fjoin", names(x))], value = NULL))
+  suppressWarnings(data.table::set(x, j = names(x)[grepl(pattern, names(x))], value = NULL))
 }
-
+# ------------------------------------------------------------------------------
+make_mock_tables <- function(on) {
+  # Create mock data.frames from an 'on' expression
+  tmp <- lapply(on, \(x) strsplit_predicate(x))
+  names_DT <- c(vapply(tmp, function(x) x[1], character(1)), "col_DT", "col_c")
+  names_i  <- c(vapply(tmp, function(x) x[3], character(1)), "col_i", "col_c")
+  .DT <- stats::setNames(as.data.frame(matrix(NA_integer_, nrow=1L, ncol=length(names_DT))), names_DT)
+  .i  <- stats::setNames(as.data.frame(matrix(NA_integer_, nrow=1L, ncol=length(names_i))), names_i)
+  list(.DT, .i)
+}
+# ------------------------------------------------------------------------------
+na_omit_cost <- function(dt) {
+  na_omit_cost_rc(nrow(dt), ncol(dt))
+}
+na_omit_cost_rc <- function(nr, nc) {
+  # Heuristic for cost of na.omit.data.table()
+  # Based on regression analysis of execution time in secs with a particular setup and machine
+  (10L + nc) * (nr / 1e9L)
+}
+# ------------------------------------------------------------------------------
+make_label_fjoin <- function(t, sub_t) {
+  # for calling in fjoin_*(): table label for printing, e.g. "x = A", "x (unnamed)"
+  paste0(deparse(substitute(t)), if (!is.null(t) & is.name(sub_t)) sprintf(" = %s", deparse(sub_t)) else " (unnamed)")
+}
+make_label_dtjoin <- function(t, sub_t) {
+  # for calling in dtjoin*(): table label for printing, e.g. "A", "(unnamed)"
+  if (!is.null(t) & is.name(sub_t)) deparse(sub_t) else "(unnamed)"
+}
 # ------------------------------------------------------------------------------
 fast_trimws <- function(x) {
   # Much faster than trimws()
   gsub("^\\s+|\\s+$", "", x)
 }
-
 # ------------------------------------------------------------------------------
 clean_on <- function(x) {
   # Standardise the spacing of a vector of 'on' expressions e.g. c("id1==id2", " date1<  date2") -> c("id2 == id1", "date2 > date1")
@@ -62,7 +82,6 @@ clean_on <- function(x) {
   )
 }
 #clean_on(c("a==b"," c   >=`hello world ` ","  e "))
-
 # ------------------------------------------------------------------------------
 flip_on <- function(x) {
   # Flip a vector of 'on' expressions e.g. c("id1==id2", "date1<date2") -> c("id2==id1", "date2>date1")
@@ -84,7 +103,6 @@ flip_on <- function(x) {
   )
 }
 #flip_on(c("a==b"," c   >=`hello world ` ","  e "))
-
 # ------------------------------------------------------------------------------
 strsplit_predicate <- function(x) {
   # Check and split a join predicate phrase e.g. "id1 > id2" -> c("id1", ">", "id2")
@@ -100,13 +118,11 @@ strsplit_predicate <- function(x) {
       fast_trimws(substr(x, pos + attr(pos, "match.length"), nchar(x))))
   }
 }
-
 #strsplit_predicate("id1>=id2")
 #strsplit_predicate("id1 >= id2")
 #strsplit_predicate("    id1 >= id2  ")
 #strsplit_predicate("id")
 #strsplit_predicate("id1 >= ")
-
 # ------------------------------------------------------------------------------
 allows_equi <- function(x) {
   # Whether operator is equality/weak inequality
@@ -114,54 +130,3 @@ allows_equi <- function(x) {
 }
 #allows_equi(">=")
 #allows_equi(">")
-
-# ------------------------------------------------------------------------------
-make_label_fjoin <- function(t, sub_t) {
-  # for calling in fjoin_*(): table label for printing, e.g. "x = A", "x (unnamed)"
-  paste0(deparse(substitute(t)), if (!is.null(t) & is.name(sub_t)) sprintf(" = %s", deparse(sub_t)) else " (unnamed)")
-}
-
-make_label_dtjoin <- function(t, sub_t) {
-  # for calling in dtjoin*(): table label for printing, e.g. "A", "(unnamed)"
-  if (!is.null(t) & is.name(sub_t)) deparse(sub_t) else "(unnamed)"
-}
-
-# ------------------------------------------------------------------------------
-make_mock_tables <- function(on) {
-  # Create mock data.frames from an 'on' expression
-  tmp <- lapply(on, \(x) strsplit_predicate(x))
-  names_DT <- c(vapply(tmp, function(x) x[1], character(1)), "col_DT", "col_c")
-  names_i  <- c(vapply(tmp, function(x) x[3], character(1)), "col_i", "col_c")
-  .DT <- stats::setNames(as.data.frame(matrix(NA_integer_, nrow=1L, ncol=length(names_DT))), names_DT)
-  .i  <- stats::setNames(as.data.frame(matrix(NA_integer_, nrow=1L, ncol=length(names_i))), names_i)
-  list(.DT, .i)
-}
-
-# ------------------------------------------------------------------------------
-na_omit_cost <- function(dt) {
-  na_omit_cost_rc(nrow(dt), ncol(dt))
-}
-
-na_omit_cost_rc <- function(nr, nc) {
-  # Heuristic for cost of na.omit()
-  # Based on regression analysis of execution time in secs with a particular setup and machine
-  (10L + nc) * (nr / 1e9L)
-}
-
-# # ------------------------------------------------------------------------------
-# dots_to_list <- function(valid_names,...) {
-#   # No longer used. Check for valid args in ... and return them as a list
-#   # note that list() evaluates the args (they are not substituted)
-#   args <- list(...)
-#   if (length(args)) {
-#     invalid_names <- setdiff(names(args), valid_names)
-#     if (length(invalid_names))
-#       stop(sprintf("Unused additional argument(s): %s. Valid additional arguments are: %s",
-#                    paste(invalid_names, collapse = ", "), paste(valid_names, collapse = ", ")))
-#   }
-#   return(args)
-# }
-#
-# # ------------------------------------------------------------------------------
-# vcat <- function(v) cat(sprintf("%s :", deparse(substitute(v))), v, "\n")
-#
